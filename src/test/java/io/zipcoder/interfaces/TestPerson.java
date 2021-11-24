49126f6fe7454e5da2d099f0a27cfa0dce6d1f50
c-poteat <nellp@mail.com>
2021-11-24 07:11:19 -0500

    Updated lab

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,41 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+
+    public void testDefaultContructorTest() {
+    // Given
+        String expectedName = "";
+        Long expectedId = null;
+
+    // When
+        Person person = new Person();
+
+    // Then
+        String actualName = person.getName();
+        Long actualId = person.getId();
+
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+
+    @Test
+    public void testSetName() {
+    // Given
+        Person person = new Person();
+        String expectedName = "Carnell";
+
+    // When
+        person.setName(expectedName);
+        String actualName = person.getName();
+
+    // Then
+        Assert.assertEquals(expectedName, actualName);
+    }
 
 }
